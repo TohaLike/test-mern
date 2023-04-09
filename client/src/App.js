78bde2843af2fc4e7f61527a74ca35719834f30f
Anton <thetoxalike444@gmail.com>
2023-04-09 13:50:12 +0300

    I fixed main bugs with register

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React from "react"
 import { BrowserRouter as Router } from "react-router-dom"
 import { useRoutes } from "./routes"
 import { useAuth } from "./hooks/auth.hook"
-import { AuthContext } from "./context/AuthContext" 
+import { AuthContext } from "./context/AuthContext"
 import 'materialize-css'
+
 
 function App() {
   const {token, login, logout, userId} = useAuth()
--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -2,7 +2,7 @@ import { createContext } from "react"
 
 function noop() {}
 
-export const AuthContext = () => createContext({
+export const AuthContext = createContext({
     token: null,
     userId: null,
     login: noop,
